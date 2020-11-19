94ae84a52a4a6a65c525495131881fd60a1d7bee
Bagmesh <61079673+Bagmesh@users.noreply.github.com>
2020-11-19 10:43:45 +0300

    Add files via upload

--- Турникет/Турникет/Form1.Designer.cs
+++ Турникет/Турникет/Form1.Designer.cs
@@ -28,60 +28,194 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button3 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.button6 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.timer5 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(107, 269);
+            this.button1.AutoSize = true;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.Location = new System.Drawing.Point(692, 179);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(96, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "Скачать БД";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(107, 206);
+            this.button2.Location = new System.Drawing.Point(33, 28);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(65, 23);
             this.button2.TabIndex = 1;
-            this.button2.Text = "Оплатить";
+            this.button2.Text = "Оплата";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(122, 159);
+            this.textBox1.Location = new System.Drawing.Point(325, 28);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(41, 20);
             this.textBox1.TabIndex = 2;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(363, 214);
+            this.button3.Location = new System.Drawing.Point(692, 108);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(142, 40);
+            this.button3.Size = new System.Drawing.Size(96, 50);
             this.button3.TabIndex = 3;
             this.button3.Text = "Добавить пользователей в БД";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.BackColor = System.Drawing.Color.Transparent;
+            this.radioButton1.Location = new System.Drawing.Point(692, 75);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(37, 17);
+            this.radioButton1.TabIndex = 13;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "50";
+            this.radioButton1.UseVisualStyleBackColor = false;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.BackColor = System.Drawing.Color.Transparent;
+            this.radioButton2.Location = new System.Drawing.Point(751, 75);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(37, 17);
+            this.radioButton2.TabIndex = 14;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "75";
+            this.radioButton2.UseVisualStyleBackColor = false;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged_1);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Silver;
+            this.button4.Location = new System.Drawing.Point(498, 86);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 18;
+            this.button4.Text = "Датчик 1";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Silver;
+            this.button5.Location = new System.Drawing.Point(498, 135);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 19;
+            this.button5.Text = "Датчик 2";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Silver;
+            this.button6.Location = new System.Drawing.Point(498, 179);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 20;
+            this.button6.Text = "Датчик 3";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Gray;
+            this.label1.Location = new System.Drawing.Point(322, 135);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Проход";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Gray;
+            this.label2.Location = new System.Drawing.Point(49, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "Стоп";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // timer4
+            // 
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // timer5
+            // 
+            this.timer5.Tick += new System.EventHandler(this.timer5_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(867, 373);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Турникет";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -93,6 +227,18 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Timer timer5;
     }
 }
 
--- Турникет/Турникет/Form1.cs
+++ Турникет/Турникет/Form1.cs
@@ -1,15 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 using System.IO;
 using System.Media;
 using System.Net;
+using System.Windows.Forms;
+
 
 namespace Турникет
 {
@@ -18,53 +13,246 @@ namespace Турникет
         public Form1()
         {
             InitializeComponent();
-        }
-        public static string cardid = "C:\\Проект Турникет\\БД пользователей.txt";
-        private WebClient wc = new WebClient();
-
-        private void button1_Click(object sender, EventArgs e)
-        {
+            timer1.Interval = 5000;
+            timer2.Interval = 5000;
+            timer3.Interval = 5000;
+            timer4.Interval = 2000;
+            timer5.Interval = 2000;
             Directory.CreateDirectory("C:\\Проект Турникет\\");
-            wc.DownloadFile("https://github.com/Bagmesh/Toorniket/raw/master/Id_polzovatelya.txt", cardid);
         }
-        public static string FindID(string s)
+        SoundPlayer spg = new SoundPlayer("C:\\Users\\mesh\\source\\repos\\Турникет\\Турникет\\spg.wav");
+        SoundPlayer spb = new SoundPlayer("C:\\Users\\mesh\\source\\repos\\Турникет\\Турникет\\spb.wav");
+        bool butt = false;
+        bool but2 = false;
+        bool but4 = false;
+        bool but5 = false;
+        bool but6 = false;
+        static string cardid = "C:\\Проект Турникет\\БД пользователей.txt";
+        WebClient wc = new WebClient();
+        int i = 1;
+        bool lb1 = false;
+        bool lb2 = false;
+        private void CloseAway()
         {
-            string str = (string)null;
-            string[] array = System.IO.File.ReadAllLines(cardid);
-            int index1 = Array.IndexOf<string>(array, s);
-            for (int index2 = 0; index2 < array.Length; ++index2)
-                str = array[index1];
+            lb2 = true;
+            timer5.Start();
+            if (!but2)
+                label2.BackColor = Color.Red;
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            butt = true;
+            wc.DownloadFile("https://github.com/Bagmesh/Toorniket/raw/master/Id_polzovatelya.txt", cardid);
+            MessageBox.Show("Скачивание успешно завершено");
+        }
+        public string FindID(string s)
+        {
+            string str = null;
+            string[] array = File.ReadAllLines(cardid);
+            int index = Array.IndexOf<string>(array, s);
+            if (butt)
+            {
+                for (int i = 0; i < array.Length; ++i)
+                    if (index < array.Length)
+                        str = array[index];
+            }
+            else
+                MessageBox.Show("Проверьте бд");
             return str;
         }
-
         private void button2_Click(object sender, EventArgs e)
         {
             if (textBox1.Text.Trim() == string.Empty)
             {
-                int num1 = (int)MessageBox.Show("Введите id карты!");
+                CloseAway();
+                spb.Play();
+                MessageBox.Show("Введите id карты!");
             }
-            if (textBox1.Text == FindID(textBox1.Text))
+            string[] array = File.ReadAllLines(cardid);
+            int num2 = Array.IndexOf<string>(array, textBox1.Text);
+            int t = Convert.ToInt32(array[num2 + 1]);
+            if (t != 0)
             {
-                string[] array = File.ReadAllLines(cardid);
-                int num2 = Array.IndexOf<string>(array, textBox1.Text);
-                array[num2 + 1] = (Convert.ToInt32(array[num2 + 1]) - 1).ToString();
-                File.Delete(cardid);
-                for (int index = 0; index < array.Length; ++index)
+                if (textBox1.Text != FindID(textBox1.Text))
                 {
-                    using (StreamWriter streamWriter = new StreamWriter(cardid, true))
-                        streamWriter.WriteLine(array[index]);
+                    CloseAway();
+                    spb.Play();
+                    MessageBox.Show("Введенный id не существуюет!");
                 }
-                MessageBox.Show("Успешно =D");
+                else
+                {
+                    array[num2 + 1] = (t - 1).ToString();
+                    File.Delete(cardid);
+                    for (int i = 0; i < array.Length; ++i)
+                    {
+                        using (StreamWriter streamWriter = new StreamWriter(cardid, true))
+                            streamWriter.WriteLine(array[i]);
+                    }
+                    spg.Play();
+                    MessageBox.Show("Успешно =D");
+                    lb1 = true;
+                    timer4.Start();
+                    if (but2)
+                        label1.BackColor = Color.Green;
+                    but2 = true;
+                }
             }
             else
             {
-                MessageBox.Show("Введенный id не существуюет!");
+                CloseAway();
+                spb.Play();
+                MessageBox.Show("Пополните карту!");
             }
         }
-
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Directory.CreateDirectory("C:\\Проект Турникет\\");
+            ++i;
+            bool T = Convert.ToBoolean(radioButton1.Checked);
+            bool G = Convert.ToBoolean(radioButton2.Checked);
+            if (T == true)
+            {
+                if (button1.Visible)
+                {
+                    if (i < 10)
+                        i = 11;
+                    using (StreamWriter streamWriter = new StreamWriter(cardid, true))
+                    {
+                        streamWriter.WriteLine(i);
+                        streamWriter.WriteLine(50);
+                    }
+                }
+                else
+                {
+                    using (StreamWriter streamWriter = new StreamWriter(cardid, true))
+                    {
+                        streamWriter.WriteLine(i);
+                        streamWriter.WriteLine(50);
+                    }
+                }
+            }
+            if (G == true)
+            {
+                if (button1.Visible)
+                {
+                    if (i < 10)
+                        i = 11;
+                    using (StreamWriter streamWriter = new StreamWriter(cardid, true))
+                    {
+                        streamWriter.WriteLine(i);
+                        streamWriter.WriteLine(75);
+                    }
+                }
+                else
+                {
+                    using (StreamWriter streamWriter = new StreamWriter(cardid, true))
+                    {
+                        streamWriter.WriteLine(i);
+                        streamWriter.WriteLine(75);
+                    }
+                }
+            }
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+        }
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+        }
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+        }
+        private void radioButton2_CheckedChanged_1(object sender, EventArgs e)
+        {
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            but4 = true;
+            timer1.Start();
+            if (but2)
+                button4.BackColor = Color.Green;
+            else
+            {
+                button4.BackColor = Color.Red;
+                spb.Play();
+            }
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            but5 = true;
+            timer2.Start();
+            if (but2)
+                button5.BackColor = Color.Green;
+            else
+            {
+                button5.BackColor = Color.Red;
+                spb.Play();
+            }
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            but6 = true;
+            timer3.Start();
+            if (but2)
+                button6.BackColor = Color.Green;
+            else
+            {
+                button6.BackColor = Color.Red;
+                spb.Play();
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (but4)
+                button4.BackColor = Color.Silver;
+            but4 = !but4;
+        }
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (but5)
+                button5.BackColor = Color.Silver;
+            but5 = !but5;
+        }
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            if (but6)
+                button6.BackColor = Color.Silver;
+            but6 = !but6;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+        protected override bool ProcessDialogKey(Keys keyData)
+        {
+            if (keyData == Keys.Escape)
+            {
+                this.Close();
+                return true;
+            }
+            else
+                return base.ProcessDialogKey(keyData);
 
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {       
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+        }
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            if (but2)
+                label1.BackColor = Color.Gray;
+            lb1 = !lb1;
+        }
+        private void timer5_Tick(object sender, EventArgs e)
+        {
+            if (but2)
+                label2.BackColor = Color.Gray;
+            lb2 = !lb2;
         }
     }
 }
